dc2da668818d1d9093d0e9732e6ed9468ca82468
unknown <232039@student.uek.krakow.pl>
2023-10-07 12:33:55 +0200

    My first Python programs from textbook

---
+++ 02-TypesAndVariables/exercise4.py
@@ -0,0 +1,11 @@
+width = 17
+height = 12.0
+first = width//2
+second = width/2.0
+third = 1 + 2 * 5
+print(first)
+print(type(first))
+print(second)
+print(type(second))
+print(third)
+print(type(third))
---
+++ 02-TypesAndVariables/exercise5.py
@@ -0,0 +1,3 @@
+cels = input("Enter Celsius temperature: ")
+fahr = float(cels) * 1.8 + 32
+print("The temperature in Fahrenheit is: ", fahr)
